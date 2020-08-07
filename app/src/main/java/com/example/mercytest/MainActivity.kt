610b533213636a6eb1edf078dc8b53d6968e6a1c
Antonio Monteiro <antoniohbmonteiro@gmail.com>
2020-08-07 19:35:22 -0300

    Organizando metodo de validação dos campos

--- app/src/main/java/com/example/mercytest/MainActivity.kt
+++ app/src/main/java/com/example/mercytest/MainActivity.kt
@@ -22,31 +22,39 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        val email : TextInputEditText = findViewById(R.id.edtLayoutEmail)
-        val senha : TextInputEditText = findViewById(R.id.edtLayoutSenha)
-
         btnEntrar.setOnClickListener {
 
-
-            if(email.text.toString().isEmpty()) {
-
-                email.error = "Coloque seu email"
+            if (validarCampos()) {
+                clickConfirmado()
             }
-
-            if (senha.text.toString().isEmpty()){
-                senha.error = "Coloque sua senha"
-            }
-
-            if(!validarEmail(email.text.toString().trim())){
-
-                email.error = "Email Invalido"
-            }else if(email.text.toString().isNotEmpty() && senha.text.toString().length > 0 ){
-
-                Toast.makeText(this,"Acesso Confirmado",Toast.LENGTH_LONG).show()
-
-            }
-
 
         }
-   }
+    }
+
+    private fun clickConfirmado() {
+        Toast.makeText(this, "Acesso Confirmado", Toast.LENGTH_LONG).show()
+    }
+
+    fun validarCampos(): Boolean {
+        val email = edtLayoutEmail.text.toString()
+        val senha = edtLayoutSenha.text.toString()
+
+        var ehValido = true
+
+        if (email.isEmpty()) {
+            ehValido = false
+            edtLayoutEmail.error = "Coloque seu email"
+        }
+        if (senha.isEmpty()) {
+            ehValido = false
+            edtLayoutSenha.error = "Coloque sua senha"
+        }
+
+        if (!validarEmail(email.trim())) {
+            ehValido = false
+            edtLayoutEmail.error = "Email Invalido"
+        }
+
+        return ehValido
+    }
 }
